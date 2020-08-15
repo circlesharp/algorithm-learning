7c28f88a7f27600f49666e79ddf380b686d94829
circlesharp <circlesharp@live.com>
2020-08-15 04:19:22 +0100

    FP: applicative programming examples

---
+++ functional/chap_2/applicative_programming_example_2.js
@@ -0,0 +1,53 @@
+const _ = require('underscore');
+
+/**
+ * find
+ */
+
+// Number 的 isNaN 只有这个值真的为 NaN 才可以
+console.log(
+  _.find(['a', 'b', 3, 'd'], Number.isNaN)
+)
+
+// 全局的 isNaN 只要不是数字都是 true
+console.log(
+  _.find(['a', 'b', 3, 'd'], isNaN)
+)
+
+// _.isNumber
+console.log(
+  _.find(['a', 'b', 3, 'd'], _.isNumber)
+)
+
+/**
+ * reject(opposite of filter / inject)
+ * also, complement
+ */
+
+console.log(
+  _.reject(['a', 'b', 3, 'd'], _.isNumber)
+)
+
+const complement = pred =>
+  function() {
+    // 太牛逼了，arguments是类数组，所以得转成数组
+    // return !pred.apply(null, Array.from(arguments));
+    return !pred.call(null, ...Array.from(arguments));
+  }
+
+console.log(
+  ['a', 'b', 3, 'd'].filter(complement(_.isNumber))
+)
+
+/**
+ * all, any === some
+ */
+
+console.log(
+  _.all([1, 2, 3, 4], _.isNumber)
+)
+
+console.log(
+  _.some([1, 2, 'c', 4], _.isNumber),
+  _.any([1, 2, 'c', 4], _.isNumber)
+)
---
+++ functional/chap_2/applicative_programming_example_3.js
@@ -0,0 +1,29 @@
+const _ = require('underscore');
+const { groupBy } = require('underscore');
+
+/**
+ * sortBy, groupBy, countBy
+ */
+
+const people = [{ name: "Rick", age: 31 }, { name: "Amy", age: 11 }, { name: "Jack", age: 24 }];
+
+console.log(
+  _.sortBy(people, 'age'),
+  _.sortBy(people, p => p.age)
+)
+
+console.log(
+  _.groupBy(people, p => p.age > 20),
+  _.groupBy(people, p => p.age > 20 ? 'old' : 'young'),
+  _.groupBy(people, p => {
+    if (p.age >= 10 && p.age < 20) return '10s';
+    if (p.age >= 20 && p.age < 30) return '20s';
+    if (p.age >= 30 && p.age < 40) return '30s';
+  })
+)
+
+console.log(
+  _.countBy(people, p =>
+    p.age > 20 ? 'old' : 'young'
+  )
+)
