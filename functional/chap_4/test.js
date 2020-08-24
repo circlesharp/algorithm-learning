91e2f30616380dba96f640c6da9a639dd5f3a034
circlesharp <circlesharp@live.com>
2020-08-24 13:16:15 +0100

    FP interset after 10 years

---
+++ functional/chap_4/test.js
@@ -0,0 +1,27 @@
+/**
+ * 复利
+ * 每个月存 1000
+ * 年利率 10%
+ * 10年后有多少钱
+ */
+
+const n = 120;
+const i = 0.1;
+const p = 1000;
+
+let total = 0;
+let t = 0;
+
+const save = (t = 0) => {
+  t++;
+  total = total * (1 + i / 12) + p;
+  if (t === n) return total;
+  return save(t);
+};
+
+const f = p * ((1 + i / 12) ** n - 1) / (1 / 12);
+
+console.log(
+  save(),
+  f,
+);
