f8b4f7f969da7e1e8a02692b5d865ee87057c462
circlesharp <circlesharp@live.com>
2020-10-25 15:47:20 +0100

    PD: 二维数组的指针

--- programDesign/05-pointer/04-pointer-2d-array.h
+++ programDesign/05-pointer/04-pointer-2d-array.h
@@ -1,11 +1,17 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
 
 void array_1d();
+void array_2d_pointer_1();
+void array_2d_pointer_2();
+void array_2d_pointer_3();
+void array_2d_pointer_4();
 
 string _main()
 {
-  array_1d();
+  // array_1d();
+  array_2d_pointer_4();
   return "04-pointer-2d-array";
 }
 
@@ -24,4 +30,72 @@ void array_1d()
 
   cout << "*a: " << *a << ", a: " << a << ", &a: " << &a << endl;
   cout << "sizeof(a[0]): " << sizeof(a[0]) << ", sizeof(a): " << sizeof(a) << ", sizeof(&a): " << sizeof(&a) << endl;
+}
+
+void array_2d_pointer_1()
+{
+  /*
+  ::: 二维数组的数组名指针 :::
+
+  1. 数组名相当于指向数组第一个元素的指针
+  2. &E 相当于把 E 的管辖范围上升一个级别
+  3. *E 相当于把 E 的管辖范围下降一个级别
+  */
+
+ int a[3][4] = {
+   {1, 2, 3, 4},
+   {5, 6, 7, 8},
+   {9, 10, 11, 12},
+ };
+ 
+  cout << "a = " << a << endl;
+  cout << "&a[0] = " << &a[0] << endl;
+  cout << "a + 1 = " << a + 1 << endl;
+  cout << "&a[0] + 1 = " << &a[0] + 1 << endl;
+
+  cout << "*a = " << *a << endl;
+  cout << "a[0] = " << a[0] << endl;
+  cout << "&a[0][0] = " << &a[0][0] << endl;
+  cout << "*a + 1 = " << *a + 1 << endl;
+  cout << "a[0] + 1 = " << a[0] + 1 << endl;
+  cout << "&a[0][0] + 1 = " << &a[0][0] + 1 << endl;
+}
+
+void array_2d_pointer_2()
+{
+  int a[3][4] = {
+    {1, 2, 3, 4},
+    {5, 6, 7, 8},
+    {9, 10, 11, 12},
+  };
+
+  cout << "a = " << a << endl;
+  cout << "&a[0] = " << &a[0] << endl;
+  cout << "a + 1 = " << a + 1 << endl;
+  cout << "&a[0] + 1 = " << &a[0] + 1 << endl << endl;
+
+  cout << "a[1] = " << a[1] << endl;
+  cout << "&a[1] = " << &a[1] << endl;
+  cout << "*(a + 1) = " << *(a + 1) << endl;
+  cout << "*a + 1 = " << *a + 1 << endl;
+  cout << "&a = " << &a << endl;
+  cout << "&a + 1 = " << &a + 1 << endl;
+}
+
+void array_2d_pointer_3()
+{
+  int a[3][4] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23};
+  int *p;
+  for (p = &a[0][0]; p < &a[0][0] + 12; p++) // 二维数组的存储也是“一字排开”
+    cout << p << ' ' << *p << endl; // 使用指针范围二维数组的元素
+}
+
+void array_2d_pointer_4()
+{
+  int a[3][4] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23};
+  int (*p)[4], i, j; // int (*p)[4]
+  p = a;
+  cin >> i >> j;
+  cout << setw(4) << p[i][j]; // 可以直接用这种语法
+  // cout << setw(4) << *(*(p + i) + j);
 }
