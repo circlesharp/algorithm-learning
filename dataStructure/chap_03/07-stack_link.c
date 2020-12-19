8631f77a0f7af2dfcdca16a6dfc9c518b33596e0
circlesharp <circlesharp@live.com>
2020-12-19 11:25:49 +0800

    12-19 link stack

--- dataStructure/chap_03/07-stack_link.c
+++ dataStructure/chap_03/07-stack_link.c
@@ -1,4 +1,4 @@
-/* 顺序栈 */
+/* 链栈 */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,10 +9,15 @@
 
 typedef int ElementType;
 
+typedef struct _SNode {
+  ElementType data;
+  struct _SNode* next;
+} SNode;
+
 typedef struct {
-  ElementType* data; /* 储存元素的数组 */
-  int top; /* 栈顶指针 */
-  int maxSize; /* 堆栈最大容量 */
+  SNode* top; /* 第一个结点就是 top */
+  int length;
+  int maxSize;
 } Stack;
 
 /* 堆栈的操作集 */
@@ -48,20 +53,20 @@ int main()
 Stack* CreateStack(int maxSize)
 {
   Stack* stack = (Stack*)malloc(sizeof(Stack));
-  stack->data = (ElementType*)malloc(maxSize * sizeof(ElementType));
-  stack->top = -1;
+  stack->top = NULL;
+  stack->length = 0;
   stack->maxSize = maxSize;
   return stack;
 }
 
 bool IsFull(Stack* stack)
 {
-  return (stack->top == stack->maxSize - 1);
+  return stack->length == stack->maxSize;
 }
 
 bool IsEmpty(Stack* stack)
 {
-  return (stack->top == -1);
+  return stack->length == 0;
 }
 
 bool Push(Stack* stack, ElementType x)
@@ -70,8 +75,16 @@ bool Push(Stack* stack, ElementType x)
     printf("The stack is full.\n");
     return false;
   }
-  stack->top += 1;
-  stack->data[stack->top] = x;
+
+  /* create sNode */
+  SNode* sNode = (SNode*)malloc(sizeof(SNode));
+  sNode->data = x;
+  sNode->next = stack->top;
+
+  /* update stack */
+  stack->top = sNode;
+  stack->length += 1;
+
   return true;
 }
 
@@ -81,14 +94,27 @@ ElementType Pop(Stack* stack)
     printf("The stack is empty.\n");
     return ERROR;
   }
-  stack->top -= 1; /* 标记 top 就可以，不必真的删除 */
-  return stack->data[stack->top];
+
+  /* update stack */
+  SNode* topNode = stack->top;
+  stack->top = topNode->next;
+  stack->length -= 1;
+  
+  /* delete sNode */
+  ElementType rst = topNode->data;
+  free(topNode);
+
+  return rst;
 }
 
 void print(Stack* stack)
 {
-  printf("The capacity of stack is: %d / %d\n", stack->top + 1, stack->maxSize);
-  for (int i = stack->top; i >= 0; i--) {
-    printf("%-4d", stack->data[i]);
+  printf("The capacity of stack is: %d / %d\n", stack->length, stack->maxSize);
+  SNode* tempNode = (SNode*)malloc(sizeof(SNode));
+  tempNode = stack->top;
+  for (int i = 0; i <= stack->length; i++) {
+    printf("%-4d", tempNode->data);
+    tempNode = tempNode->next;
   }
+  free(tempNode);
 }
