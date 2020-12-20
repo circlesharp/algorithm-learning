744e3605098bc4727deb67a6bd83884491ae06d4
circlesharp <circlesharp@live.com>
2020-12-20 13:01:10 +0800

    12-20 link queue

--- dataStructure/chap_03/09-queue_link.c
+++ dataStructure/chap_03/09-queue_link.c
@@ -1,4 +1,4 @@
-/* 队列 顺序结构 */
+/* 队列 链式存储 */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,10 +9,15 @@
 
 typedef int ElementType;
 
+typedef struct _QNode {
+  ElementType data;
+  struct _QNode* next;
+} QNode;
+
 typedef struct {
-  ElementType* data;
-  int front;
-  int rear;
+  QNode* front;
+  QNode* rear;
+  int length;
   int maxSize;
 } Queue;
 
@@ -23,6 +28,7 @@ bool IsEmpty(Queue* queue);
 ElementType DeleteQ(Queue* queue);
 void print(Queue* queue);
 
+// ===================
 int main()
 {
   Queue* queue = CreateQueue(MAXSIZE);
@@ -43,6 +49,7 @@ int main()
   DeleteQ(queue);
   print(queue);
 
+  /* 链式存储比顺序存储能多容纳1个 */
   AddQ(queue, 199);
   AddQ(queue, 199);
   AddQ(queue, 199);
@@ -55,35 +62,44 @@ int main()
 Queue* CreateQueue(int maxSize)
 {
   Queue* queue = (Queue*)malloc(sizeof(Queue));
-  queue->data = (ElementType*)malloc(maxSize * sizeof(ElementType));
-  queue->front = 0;
-  queue->rear = 0;
+  queue->front = NULL;
+  queue->rear = NULL;
+  queue->length = 0;
   queue->maxSize = maxSize;
-
-  return queue;
 }
 
 bool IsFull(Queue* queue)
 {
-  return (queue->rear + 1) % queue->maxSize == queue->front;
+  return queue->length == queue->maxSize;
 }
 
-bool AddQ(Queue* queue, ElementType x)
+bool IsEmpty(Queue* queue)
 {
+  return queue->length == 0;
+}
+
+bool AddQ(Queue* queue, ElementType x){
   if (IsFull(queue)) {
     printf("The queue is full.\n");
     return false;
   }
 
-  queue->rear = (queue->rear + 1) % queue->maxSize;
-  queue->data[queue->rear] = x;
+  /* 处理结点 */
+  QNode* qNode = (QNode*)malloc(sizeof(QNode));
+  qNode->data = x;
+  qNode->next = NULL;
 
-  return true;
-}
+  /* 处理队列 */
+  if (IsEmpty(queue)) {
+    queue->front = qNode;
+    queue->rear = qNode;
+  }
+  else {
+    queue->rear->next = qNode;
+    queue->rear = qNode;
+  }
 
-bool IsEmpty(Queue* queue)
-{
-  return queue->rear == queue->front;
+  queue->length += 1;
 }
 
 ElementType DeleteQ(Queue* queue)
@@ -93,27 +109,26 @@ ElementType DeleteQ(Queue* queue)
     return ERROR;
   }
 
-  queue->front = (queue->front + 1) % queue->maxSize;
-  return queue->data[queue->front];
+  /* 处理队列 */
+  QNode* tempNode = queue->front;
+  queue->front = tempNode->next;
+  queue->length -= 1;
+
+  /* 处理头结点 */
+  ElementType rst = tempNode->data;
+  free(tempNode);
+  return rst;
 }
 
 void print(Queue* queue)
 {
-  if (queue->front > queue->rear) {
-    for (int i = queue->front + 1; i <= queue->maxSize + queue->rear; i++) {
-      printf("%-4d", queue->data[i % queue->maxSize]);
-    }
-  }
+  if (IsEmpty(queue))
+    printf("The queue is empty.");
 
-  else if (queue->front < queue->rear) {
-    for (int i = queue->front + 1; i <= queue->rear; i++) {
-      printf("%-4d", queue->data[i]);
-    }
-  }
-
-  /* IsEmpty(queue) */
-  else {
-    printf("The queue is empty.\n");
+  QNode* tempNode = queue->front;
+  while (tempNode) {
+    printf("%-4d", tempNode->data);
+    tempNode = tempNode->next;
   }
 
   printf("\nprint end.\n");
