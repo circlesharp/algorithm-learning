a6e0d4744330280f1f19ea3c1c98ce67d50f4311
circlesharp <circlesharp@live.com>
2020-12-22 22:30:42 +0800

    12-22 binary tree traversal

--- dataStructure/chap_04/06-binary_tree_link.c
+++ dataStructure/chap_04/06-binary_tree_link.c
@@ -15,6 +15,10 @@ typedef struct _BinTree {
 
 bool IsEmpty(BinTree* binTree);
 void Traversal(BinTree* binTree); /* 二叉树遍历 */
+void InorderTraversal(BinTree* binTree); /* 中序遍历 LVR */
+void PreorderTraversal(BinTree* binTree); /* 先序遍历 VLR */
+void PostorderTraversal(BinTree* binTree); /* 后序遍历 LRV */
+void LevelorderTraversal(BinTree* binTree); /* 层序遍历 LRV */
 BinTree* CreateBinTree();
 
 int main()
@@ -22,9 +26,42 @@ int main()
   ElementType treeData[] = {1, 2, 3, 4, 5, 6, 7, 0, 0, 10, 0, 0, 13, 0, 0, 0, 0, 0, 0};
   BinTree* binTree = CreateBinTree(treeData, sizeof(treeData) / sizeof(treeData[0]));
 
+  
+  {
+    /* test 生成 */
+    printf("1: %d\n", binTree->data);
+    printf("4: %d\n", binTree->left->left->data);
+    printf("10: %d\n", binTree->left->right->left->data);
+  }
+
+  {
+    /* test IsEmpty */
+    printf("empty? %d\n", IsEmpty(binTree));
+    printf("empty? %d\n", IsEmpty(binTree->left->left));
+    printf("empty? %d\n", IsEmpty(binTree->left->left->left));
+  }
+
+  {
+    /* Traversal */
+    InorderTraversal(binTree);
+    printf("\n");
+    PreorderTraversal(binTree);
+    printf("\n");
+    PostorderTraversal(binTree);
+    printf("\n");
+    LevelorderTraversal(binTree);
+    printf("\n");
+  }
+
   return 0;
 }
 
+bool IsEmpty(BinTree* binTree)
+{
+  return binTree == NULL;
+}
+
+/* 层序生成二叉树 */
 BinTree* CreateBinTree(ElementType* treeData, int dataLength)
 {
   /* 若第一位是 NoInfo，空树 */
@@ -41,7 +78,7 @@ BinTree* CreateBinTree(ElementType* treeData, int dataLength)
   bool isLeftChild = true;
   AddQ(nodeQueue, bTree);
 
-
+  /* 可以尝试 i += 2 的循环，但是不安全，所以还是区分左右好一点 */
   for (int i = 1; i < dataLength; i++) {
     /* 处理队列 */
     if (isLeftChild)
@@ -67,4 +104,56 @@ BinTree* CreateBinTree(ElementType* treeData, int dataLength)
 
   printf("the loop is over\n");
   return bTree;
+}
+
+/* 二叉树遍历(中序) */
+void Traversal(BinTree* binTree)
+{
+  // 需要用堆栈实现
+}
+
+
+/* 中序遍历 LVR */
+void InorderTraversal(BinTree* binTree)
+{
+  if (binTree == NULL) return;
+  InorderTraversal(binTree->left);
+  printf("%-6d", binTree->data);
+  InorderTraversal(binTree->right);
+}
+
+/* 先序遍历 VLR */
+void PreorderTraversal(BinTree* binTree)
+{
+  if (binTree == NULL) return;
+  printf("%-6d", binTree->data);
+  InorderTraversal(binTree->left);
+  InorderTraversal(binTree->right);
+}
+
+/* 后序遍历 LRV */
+void PostorderTraversal(BinTree* binTree)
+{
+  if (binTree == NULL) return;
+  InorderTraversal(binTree->left);
+  InorderTraversal(binTree->right);
+  printf("%-6d", binTree->data);
+}
+
+/* 层序遍历 LRV */
+void LevelorderTraversal(BinTree* binTree)
+{
+  if (binTree == NULL) return;
+
+  /* 创建队列 */
+  Queue* nodeQueue = CreateQueue(100);
+  AddQ(nodeQueue, binTree);
+
+  /* 遍历 */
+  while (!IsQueueEmpty(nodeQueue)) {
+    BinTree* tempTree = DeleteQ(nodeQueue);
+    printf("%-6d", tempTree->data);
+    if (tempTree->left) AddQ(nodeQueue, tempTree->left);
+    if (tempTree->right) AddQ(nodeQueue, tempTree->right);
+  }
 }
