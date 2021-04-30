4ffe7b26076e353a54b0367b32692c83baabecfd
circlesharp <circlesharp@live.com>
2021-04-30 18:43:46 +0800

    feat(tree): binary-tree-link
    sub
    
    sub 层序遍历 LevelOrderTraversal
    
    sub VLR, LVR, LRV

--- tsDataStructure/src/chap_4/BinaryTree.ts
+++ tsDataStructure/src/chap_4/BinaryTree.ts
@@ -1,9 +1,10 @@
 import { QueueLink } from '../chap_3/Queue';
 
+// ================ type: 定义 node ================
 type BTreeNodeSeq<T> = {
   data: T;
   idx: number;
-}
+};
 
 class BTreeNodeLink<T> {
   data: T;
@@ -17,6 +18,9 @@ class BTreeNodeLink<T> {
 
 type BTreeNode<T> = BTreeNodeSeq<T> | BTreeNodeLink<T>;
 
+type traversalType = 'inOrder' | 'preOrder' | 'postOrder' | 'levelOrder';
+
+// ================ type: 定义 tree ================
 interface BTreeSeq<T> {
   data: Array<BTreeNode<T>>;
   length: number;
@@ -27,14 +31,13 @@ interface BTreeSeq<T> {
   toArray: () => Array<T>;
 }
 
-type traversalType = 'inOrder' | 'preOrder' | 'postOrder' | 'levelOrder';
-
 interface BTreeLink<T> {
-  data: BTreeNodeLink<T>;
+  root: BTreeNodeLink<T>;
   isEmpty: () => boolean;
-  traversal: (node: BTreeNodeLink<T>, type: traversalType) => void;
+  traversal: (type: traversalType) => void;
 }
 
+// ================ code 二叉树 顺序表实现 ================
 export class BinaryTreeSeq<T> implements BTreeSeq<T> {
   data = [null]; // 第0位用null占位
   length: number;
@@ -44,7 +47,7 @@ export class BinaryTreeSeq<T> implements BTreeSeq<T> {
       const node: BTreeNode<T> = {
         data: data[i - 1],
         idx: i,
-      }
+      };
       this.data.push(node);
     }
 
@@ -56,35 +59,36 @@ export class BinaryTreeSeq<T> implements BTreeSeq<T> {
       return null;
     }
     return this.data[index];
-  }
+  };
 
   getParent = (index) => {
     const parentIdx = Math.floor(index / 2);
     return this.findKthNode(parentIdx);
-  }
+  };
 
   getChildLeft = (index) => {
     const childLeftIdx = index * 2;
     return this.findKthNode(childLeftIdx);
-  }
+  };
 
   getChildRight = (index) => {
     const childRightIdx = index * 2 + 1;
     return this.findKthNode(childRightIdx);
-  }
+  };
 
   toArray = () => {
     const arr = this.data.slice(1).map(i => i.data);
     return arr;
-  }
+  };
 };
 
+// ================ code 二叉树 链表实现 ================
 export class BinaryTreeLink<T> implements BTreeLink<T> {
-  data: BTreeNodeLink<T>;
+  root: BTreeNodeLink<T>;
 
   constructor(data: Array<T> = []) {
     if (!data.length) {
-      this.data = null;
+      this.root = null;
       return;
     }
 
@@ -94,15 +98,20 @@ export class BinaryTreeLink<T> implements BTreeLink<T> {
 
     /* 处理根节点 */
     tmpRoot = new BTreeNodeLink(data[0]);
-    this.data = tmpRoot;
+    this.root = tmpRoot;
     queue.addQ(tmpRoot);
 
-    /* 处理其他节点 */
+    /* 处理其他节点: 既要插入队列，又要挂载到父节点上 */
+    /* 保证每个 data 数组的每个值都有父节点 */
+    /* 不保证队列会清空，未清空的就是叶节点 */
     for (let i = 1; i < data.length; i++) {
       if (shouldAddLeft) {
+        /* 说明轮到新的 root, 需要从队列中取出新的节点，并更新 root */
         tmpRoot = queue.deleteQ();
       }
 
+      /* 产生子节点: 要么是合法的node, 要么是空指针 */
+      /* 如果是合法的node, 添加到队列，因为可能是某个子树的根节点 */
       let tmpNode: BTreeNodeLink<T>;
       if (data[i] == null) {
         tmpNode = null;
@@ -111,21 +120,102 @@ export class BinaryTreeLink<T> implements BTreeLink<T> {
         queue.addQ(tmpNode);
       }
 
+      /* 挂载到父节点的正确位置上 */
       if (shouldAddLeft) {
         tmpRoot.left = tmpNode;
       } else {
         tmpRoot.right = tmpNode;
       }
 
+      /* 切换左右 */
       shouldAddLeft = !shouldAddLeft;
     }
   }
 
   isEmpty = () => {
-    return true;
+    return BinaryTreeLink.IsEmpty(this.root);
+  };
+
+  static IsEmpty<T>(root: BTreeNodeLink<T>): boolean {
+    return !root;
   }
 
-  traversal = (node = this.data, type = 'inOrder') => {
-    return [];
+  traversal = (type: traversalType = 'inOrder') => {
+    switch (type) {
+      case 'inOrder':
+        return BinaryTreeLink.InOrderTraversal(this.root);
+      case 'preOrder':
+        return BinaryTreeLink.PreOrderTraversal(this.root);
+      case 'postOrder':
+        return BinaryTreeLink.PostOrderTraversal(this.root);
+      case 'levelOrder':
+        return BinaryTreeLink.LevelOrderTraversal(this.root);
+      default:
+        return [];
+    }
+  };
+
+  static InOrderTraversal<T>(root: BTreeNodeLink<T>): Array<T> {
+    const rst: Array<T> = [];
+    BinaryTreeLink._inOrderTraversal(root, rst);
+    return rst;
+  }
+  static PreOrderTraversal<T>(root: BTreeNodeLink<T>): Array<T> {
+    const rst: Array<T> = [];
+    BinaryTreeLink._preOrderTraversal(root, rst);
+    return rst;
+  }
+  static PostOrderTraversal<T>(root: BTreeNodeLink<T>): Array<T> {
+    const rst: Array<T> = [];
+    BinaryTreeLink._postOrderTraversal(root, rst);
+    return rst;
+  }
+  static LevelOrderTraversal<T>(root: BTreeNodeLink<T>): Array<T> {
+    const rst: Array<BTreeNodeLink<T>> = [];
+    const queue = new QueueLink([], 100);
+    let tmpNode = root;
+
+    while (tmpNode) {
+      rst.push(tmpNode);
+      if (tmpNode.left) {
+        queue.addQ(tmpNode.left);
+      }
+      if (tmpNode.right) {
+        queue.addQ(tmpNode.right);
+      }
+
+      try {
+        tmpNode = queue.deleteQ();
+      } catch (err) {
+        tmpNode = null;
+      }
+    }
+
+    return rst.map(node => node.data);
+  }
+
+  /* 中序遍历 VLR */
+  static _inOrderTraversal<T>(root: BTreeNodeLink<T>, rst: Array<T>) {
+    if (root) {
+      BinaryTreeLink._inOrderTraversal(root.left, rst);
+      rst.push(root.data);
+      BinaryTreeLink._inOrderTraversal(root.right, rst);
+    }
+  }
+  /* 先序遍历 LVR */
+  static _preOrderTraversal<T>(root: BTreeNodeLink<T>, rst: Array<T>) {
+    if (root) {
+      rst.push(root.data);
+      BinaryTreeLink._preOrderTraversal(root.left, rst);
+      BinaryTreeLink._preOrderTraversal(root.right, rst);
+    }
+  }
+  /* 后序遍历 LRV */
+  static _postOrderTraversal<T>(root: BTreeNodeLink<T>, rst: Array<T>) {
+    if (root) {
+      BinaryTreeLink._postOrderTraversal(root.left, rst);
+      BinaryTreeLink._postOrderTraversal(root.right, rst);
+      rst.push(root.data);
+    }
   }
 }
--- tsDataStructure/src/chap_4/test/BinaryTree.spec.ts
+++ tsDataStructure/src/chap_4/test/BinaryTree.spec.ts
@@ -1,17 +1,73 @@
 import { genOrderedIntArray } from "../../utils/testTools";
 import { BinaryTreeSeq, BinaryTreeLink } from '../BinaryTree';
 
-// describe('Binary Tree Link', () => {
-//   const size = 20;
-//   const arr = genOrderedIntArray(size, 1);
-//   const bTree = new BinaryTreeLink(arr);
+describe('Binary Tree Link', () => {
+  const size = 20;
+  const arr = genOrderedIntArray(size, 1);
+  const bTree = new BinaryTreeLink(arr);
 
-//   it('初始化', () => {
-//     expect(bTree.data.data).toEqual(1);
-//     expect(bTree.data.left.data).toEqual(2);
-//     expect(bTree.data.right.data).toEqual(3);
-//   });
-// });
+  const bTreeEmpty = new BinaryTreeLink();
+
+  it('初始化', () => {
+    expect(bTree.root.data).toEqual(1);
+    expect(bTree.root.left.data).toEqual(2);
+    expect(bTree.root.right.data).toEqual(3);
+
+    expect(bTree.root.left.left.data).toEqual(4);
+    expect(bTree.root.left.right.data).toEqual(5);
+
+    expect(bTree.root.left.right.left.left.data).toEqual(20);
+    expect(bTree.root.left.right.left.right).toEqual(null);
+  });
+
+  it('isEmpty', () => {
+    expect(bTree.isEmpty()).toEqual(false);
+    expect(bTreeEmpty.isEmpty()).toEqual(true);
+
+    expect(BinaryTreeLink.IsEmpty(bTree.root.right.right.right.right)).toEqual(true);
+  });
+
+  it('层序遍历 LevelOrderTraversal', () => {
+    expect(BinaryTreeLink.LevelOrderTraversal(bTree.root)).toEqual(arr);
+    expect(BinaryTreeLink.LevelOrderTraversal(bTree.root.left)).toEqual([2, 4, 5, 8, 9, 10, 11, 16, 17, 18, 19, 20]);
+    expect(BinaryTreeLink.LevelOrderTraversal(bTree.root.right)).toEqual([3, 6, 7, 12, 13, 14, 15]);
+
+    expect(BinaryTreeLink.LevelOrderTraversal(bTreeEmpty.root)).toEqual([]);
+  });
+
+  it('中序遍历 InOrderTraversal', () => {
+    const LVR = [16, 8, 17, 4, 18, 9, 19, 2, 20, 10, 5, 11, 1, 12, 6, 13, 3, 14, 7, 15];
+    expect(BinaryTreeLink.InOrderTraversal(bTree.root)).toEqual(LVR);
+    expect(bTree.traversal('inOrder')).toEqual(LVR);
+
+    const rstOfInOrderTraversal_2 = [12, 6, 13, 3, 14, 7, 15];
+    expect(BinaryTreeLink.InOrderTraversal(bTree.root.right)).toEqual(rstOfInOrderTraversal_2);
+
+    expect(bTreeEmpty.traversal('inOrder')).toEqual([]);
+
+    expect(BinaryTreeLink.InOrderTraversal(bTree.root.right.right.right)).toEqual([15]);
+  });
+
+  it('先序遍历 PreOrderInOrderTraversal', () => {
+    const VLR = [1, 2, 4, 8, 16, 17, 9, 18, 19, 5, 10, 20, 11, 3, 6, 12, 13, 7, 14, 15];
+    expect(BinaryTreeLink.PreOrderTraversal(bTree.root)).toEqual(VLR);
+    expect(bTree.traversal('preOrder')).toEqual(VLR);
+
+    expect(bTreeEmpty.traversal('preOrder')).toEqual([]);
+
+    expect(BinaryTreeLink.PreOrderTraversal(bTree.root.right.right.right)).toEqual([15]);
+  });
+
+  it('后序遍历 PostOrderTraversal', () => {
+    const LRV = [16, 17, 8, 18, 19, 9, 4, 20, 10, 11, 5, 2, 12, 13, 6, 14, 15, 7, 3, 1];
+    expect(BinaryTreeLink.PostOrderTraversal(bTree.root)).toEqual(LRV);
+    expect(bTree.traversal('postOrder')).toEqual(LRV);
+
+    expect(bTreeEmpty.traversal('postOrder')).toEqual([]);
+
+    expect(BinaryTreeLink.PostOrderTraversal(bTree.root.right.right.right)).toEqual([15]);
+  });
+});
 
 describe('Binary Tree Seq', () => {
   const size = 20;
